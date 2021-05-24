e158ee48b6ea44aad1ded028fe5da9ed3ea0b2a1
Jan Oleksik <222529@edu.p.lodz.pl>
2021-05-24 17:53:16 +0200

    Dodanie testu rzucajacego wyjatek DataIntegrityViolationException

--- blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -47,6 +48,22 @@ class BlogApiTest {
                                       .content(content))
            .andExpect(status().isCreated())
            .andExpect(content().string(writeJson(new Id(newUserId))));
+    }
+
+    @Test
+    public void postBlogUserShouldThrowDataIntegrityViolationException() throws Exception {
+        Long newUserId = 1L;
+        UserRequest user = new UserRequest();
+        user.setEmail("john@domain.com");
+        user.setFirstName("John");
+        user.setLastName("Steward");
+        when(blogService.createUser(user)).thenThrow(DataIntegrityViolationException.class);
+        String content = writeJson(user);
+
+        mvc.perform(post("/blog/user").contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(status().isConflict());
     }
 
     private String writeJson(Object obj) throws JsonProcessingException {
